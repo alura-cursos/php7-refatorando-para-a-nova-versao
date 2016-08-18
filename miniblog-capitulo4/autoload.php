dad999244ea50a624caffd6c531c7db5ae3782df
caiocmsouza <caiocesar.msouza@gmail.com>
2016-08-18 16:43:25 -0300

    projeto final

--- miniblog-capitulo4/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	function carregaClasse($nome){
-		$ds = DIRECTORY_SEPARATOR;
-		$caminho = str_replace("\\", $ds, "$nome.php");
-		if(file_exists($caminho)){
-		include "$caminho";
-		}
-	}
-	spl_autoload_register("carregaClasse");
